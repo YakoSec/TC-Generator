70de5016b7e6e0121c2eaffc6c13b2265477648a
YakoSec <93792838+YakoSec@users.noreply.github.com>
2023-02-08 15:59:01 +0330

    Update TC Generator.py

--- TC Generator.py
+++ TC Generator.py
@@ -1,12 +1,16 @@
 # Coder = Mr. Yako
 # Instagram = @YakoSec
+# GitHub : @YakoSec
 
 import random
+
 import colorama
-from colorama import Fore, Back, Style
+from colorama import Fore,Back, Style
 colorama.init()
+
 print(Fore.YELLOW)
 print("Coder: Mr. Yako \nInstagram: @YakoSec")
+
 x = random.randint(1000000000, 100000000000)
 print(Fore.RED)
 print("TC Kimlik numaranız oluşturuldu:" , x)
